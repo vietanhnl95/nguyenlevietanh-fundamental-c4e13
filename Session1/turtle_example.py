b793484de2ca4486ecda56ec7094ccc1f9a6cfca
someone <someone@someplace.com>
2017-10-31 01:08:20 +0700

    some init msg

---
+++ Session1/turtle_example.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+shape("turtle")
+
+for i in range(3):
+    for a in range(4):
+        forward(100)
+        left(90)
+
+    right(30)
+
+
+mainloop()
---
+++ Session2/Maze.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+speed(-1)
+for i in range(0,500,5):
+    forward(i)
+    left(90)
+
+mainloop()
